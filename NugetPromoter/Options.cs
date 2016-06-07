0144325eb0cb096f332fd0b9bd7610d95bb5237f
Jochen Van Gasse <jochenvangasse@telenet.be>
2016-06-07 14:19:18 +0200

    initial commit

---
+++ NugetPromoter/Options.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace NugetPromoter
+{
+    class Options
+    {
+        [Option('p', "package", Required = true, HelpText = "Path to the package to promote")]
+        public string PackageToPromote { get; set; }
+
+        [Option('o', "outputpath", Required = false, HelpText = "Output path")]
+        public string OutputPath { get; set; }
+
+        [Option('t', "tag", Required = false, HelpText = "An optional new prerelease tag to be applied on the new package")]
+        public string PreleaseTag { get; set; }
+    }
+}
---
+++ NugetPromoter/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NuGet;
+
+namespace NugetPromoter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var options = new Options();
+            var result = CommandLine.Parser.Default.ParseArguments(args, options);
+
+            if (!result)
+                throw new NotSupportedException("Invalid arguments");
+
+
+            if (!File.Exists(options.PackageToPromote))
+                throw new FileNotFoundException("Cannot find the file", options.PackageToPromote);
+
+            var package = new ZipPackage(options.PackageToPromote);
+            var temppath = Path.GetTempPath() + Guid.NewGuid();
+            package.ExtractContents(new PhysicalFileSystem(temppath), "");
+
+            Console.WriteLine("Package Id: " + package.Id);
+            Console.WriteLine($"Extracted to {temppath}");
+
+            var match = Regex.Match(package.Version.ToString(), @"\d+\.\d+\.\d+");
+            if (!match.Success)
+                throw new NotSupportedException("No semantic version was found in the metadata of the package.");
+
+            string newVersion = match.Value;
+            Console.WriteLine($"The new version of the package will be {newVersion}");
+            var metadata = new ManifestMetadata
+            {
+                Id = package.Id,
+                Version = newVersion,
+                Title = package.Title,
+                ReleaseNotes = package.ReleaseNotes,
+                Summary = package.Summary,
+                Description = package.Description,
+                Copyright = package.Copyright,
+                Language = package.Language
+            };
+            if (package.IconUrl != null) metadata.IconUrl = package.IconUrl.ToString();
+            if (package.LicenseUrl != null) metadata.LicenseUrl = package.LicenseUrl.ToString();
+            if (package.ProjectUrl != null) metadata.ProjectUrl = package.ProjectUrl.ToString();
+            if (package.Owners != null) metadata.Owners = string.Join(", ", package.Owners);
+            if (package.Authors != null) metadata.Authors = string.Join(", ", package.Authors);
+
+            var builder = new PackageBuilder();
+            var files = Directory.GetFiles(temppath, "*", SearchOption.AllDirectories)
+                            .Where(f => !f.EndsWith(".nuspec"))
+                            .Select(f => new ManifestFile { Source = f, Target = f.Replace(temppath, "") })
+                            .ToList();
+            builder.PopulateFiles("", files);
+            builder.Populate(metadata);
+
+            if (string.IsNullOrEmpty(options.OutputPath))
+                options.OutputPath = Directory.GetCurrentDirectory();
+
+            var outputFile = Path.Combine(options.OutputPath, $"{package.Id}.{newVersion}.nupkg");
+            Console.WriteLine($"Saving new file to {outputFile}");
+            using (var stream = File.Open(outputFile, FileMode.Create))
+            {
+                builder.Save(stream);
+            }
+
+            Console.WriteLine("Succesfully promoted package");
+        }
+    }
+}
